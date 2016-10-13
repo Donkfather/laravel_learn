46c9d7ea81af03349f60793e0c83a7b1ea8fe088
Tiberiu Adrian Viziru <c026vizit@RBRO.RBG.CC>
2016-10-13 18:36:24 +0300

    First commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js">
+
+</head>
+<body>
+
+	<div class="container">
+		
+		@yield('content')
+
+	</div>
+
+	@yield('footer')
+
+</body>
+</html>
